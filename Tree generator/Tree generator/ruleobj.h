f8d6b1b7cfe9ea21a0efcf1f1b45631020c8dc12
Jared Vanotterdyk <oracleryuu@csu.fullerton.edu>
2017-05-09 17:56:14 -0700

    started populating rule array and fixed symbol array

--- Tree generator/Tree generator/ruleobj.h
+++ Tree generator/Tree generator/ruleobj.h
@@ -4,6 +4,7 @@ using namespace std;
 
 
 class ruleobj {
+public:
 	ruleobj(int ID, int _lhs, int count) {
 		ruleID = ID;	lhs = _lhs; kidCount = count;
 	}
@@ -18,6 +19,51 @@ public:
 	int getLhs() { return lhs; }
 	int getRhs(int slot) { return rhs[slot]; }
 	int getCount() { return kidCount; }
-	void setRhs(int slot, int inputrule) { rhs[slot] = inputrule; }
+	void setRhs(int slot, int inputsym) { rhs[slot] = inputsym; }
 
 };
+ruleobj* ruleArray[33];
+
+void populateRuleArray() 
+{
+	ruleArray[0] = new ruleobj(0, 0, 0);
+
+	ruleArray[1] = new ruleobj(1, 24, 2);
+	ruleArray[1]->setRhs(0, 1);
+	ruleArray[1]->setRhs(1, 25);
+
+	ruleArray[2] = new ruleobj(2, 25, 3);
+	ruleArray[2]->setRhs(0, 8);
+	ruleArray[2]->setRhs(1, 26);
+	ruleArray[3]->setRhs(2, 9);
+
+	ruleArray[3] = new ruleobj(3, 26, 3);
+	ruleArray[3]->setRhs(0, 27);
+	ruleArray[3]->setRhs(1, 12);
+	ruleArray[3]->setRhs(2, 26);
+
+	ruleArray[4] = new ruleobj(4, 27, 1);
+	ruleArray[4]->setRhs(0, 30);
+
+	ruleArray[5] = new ruleobj(5, 27, 1);
+	ruleArray[5]->setRhs(0, 31);
+
+	ruleArray[6] = new ruleobj(6, 27, 1);
+	ruleArray[6]->setRhs(0, 32);
+
+	ruleArray[7] = new ruleobj(7, 27, 1);
+	ruleArray[7]->setRhs(0, 28);
+
+	ruleArray[8] = new ruleobj(8, 28, 3);
+	ruleArray[8]->setRhs(0, 18);
+	ruleArray[8]->setRhs(1, 23);
+	ruleArray[8]->setRhs(2, 29);
+
+	ruleArray[9] = new ruleobj(9, 29, 1);
+	ruleArray[9]->setRhs(0, 2);
+
+	ruleArray[10] = new ruleobj(10, 29, 1);
+	ruleArray[10]->setRhs(0, 36);
+
+
+}
--- Tree generator/Tree generator/symObj.h
+++ Tree generator/Tree generator/symObj.h
@@ -13,57 +13,55 @@ public:
 };
 
 
-symObj* symArrayT[24];
-symObj* symArrayNT[21];
-void populateSymArrayT(){
-	symArrayT[0] = new symObj(0, "epsilon", 1);
-	symArrayT[1] = new symObj(1, "prog", 1);
-	symArrayT[2] = new symObj(2, "input", 1);
-	symArrayT[3] = new symObj(3, "print", 1);
-	symArrayT[4] = new symObj(4, "while", 1);
-	symArrayT[5] = new symObj(5, "if", 1);
-	symArrayT[6] = new symObj(6, "elseif", 1);
-	symArrayT[7] = new symObj(7, "else", 1);
-	symArrayT[8] = new symObj(8, "brace1", 1);
-	symArrayT[9] = new symObj(9, "brace2", 1);
-	symArrayT[10] = new symObj(10, "paren1", 1);
-	symArrayT[11] = new symObj(11, "paren2", 1);
-	symArrayT[12] = new symObj(12, "semicolon", 1);
-	symArrayT[13] = new symObj(13, "comma", 1);
-	symArrayT[14] = new symObj(14, "id", 1);
-	symArrayT[15] = new symObj(15, "int", 1);
-	symArrayT[16] = new symObj(16, "float", 1);
-	symArrayT[17] = new symObj(17, "string", 1);
-	symArrayT[18] = new symObj(181, "plus", 1);
-	symArrayT[19] = new symObj(19, "minus", 1);
-	symArrayT[20] = new symObj(20, "aster", 1);
-	symArrayT[21] = new symObj(21, "slash", 1);
-	symArrayT[22] = new symObj(22, "caret", 1);
-	symArrayT[23] = new symObj(23, "equal", 1);
+symObj* symArray[24];
 
-}
-void populateSymArrayNT() {
-	symArrayNT[0] = new symObj(0, "PGM", 0);
-	symArrayNT[1] = new symObj(1, "BLOCK", 0);
-	symArrayNT[2] = new symObj(2, "STMTS", 0);
-	symArrayNT[3] = new symObj(3, "STMT", 0);
-	symArrayNT[4] = new symObj(4, "ASTMT", 0);
-	symArrayNT[5] = new symObj(5, "Y", 0);
-	symArrayNT[6] = new symObj(6, "OSTMT", 0);
-	symArrayNT[7] = new symObj(7, "WSTMT", 0);
-	symArrayNT[8] = new symObj(8, "FSTMT", 0);
-	symArrayNT[9] = new symObj(9, "ELSE2", 0);
-	symArrayNT[11] = new symObj(11, "ELIST", 0);
-	symArrayNT[12] = new symObj(12, "ELIST2", 0);
-	symArrayNT[13] = new symObj(13, "E", 0);
-	symArrayNT[14] = new symObj(14, "Q", 0);
-	symArrayNT[15] = new symObj(15, "T", 0);
-	symArrayNT[16] = new symObj(16, "R", 0);
-	symArrayNT[17] = new symObj(17, "F", 0);
-	symArrayNT[18] = new symObj(181, "PERXPR", 0);
-	symArrayNT[19] = new symObj(19, "FATOM", 0);
-	symArrayNT[20] = new symObj(20, "OPADD", 0);
-	symArrayNT[21] = new symObj(21, "OPMUL", 0);
+void populatesymArray(){
+	symArray[0] = new symObj(0, "epsilon", 1);
+	symArray[1] = new symObj(1, "prog", 1);
+	symArray[2] = new symObj(2, "input", 1);
+	symArray[3] = new symObj(3, "print", 1);
+	symArray[4] = new symObj(4, "while", 1);
+	symArray[5] = new symObj(5, "if", 1);
+	symArray[6] = new symObj(6, "elseif", 1);
+	symArray[7] = new symObj(7, "else", 1);
+	symArray[8] = new symObj(8, "brace1", 1);
+	symArray[9] = new symObj(9, "brace2", 1);
+	symArray[10] = new symObj(10, "paren1", 1);
+	symArray[11] = new symObj(11, "paren2", 1);
+	symArray[12] = new symObj(12, "semi", 1);
+	symArray[13] = new symObj(13, "comma", 1);
+	symArray[14] = new symObj(14, "id", 1);
+	symArray[15] = new symObj(15, "int", 1);
+	symArray[16] = new symObj(16, "float", 1);
+	symArray[17] = new symObj(17, "string", 1);
+	symArray[18] = new symObj(181, "plus", 1);
+	symArray[19] = new symObj(19, "minus", 1);
+	symArray[20] = new symObj(20, "aster", 1);
+	symArray[21] = new symObj(21, "slash", 1);
+	symArray[22] = new symObj(22, "caret", 1);
+	symArray[23] = new symObj(23, "equal", 1);
+	//nonterminals start here -----------------------------------------------------------
+	symArray[24] = new symObj(0, "PGM", 0);
+	symArray[25] = new symObj(25, "BLOCK", 0);
+	symArray[26] = new symObj(26, "STMTS", 0);
+	symArray[27] = new symObj(27, "STMT", 0);
+	symArray[28] = new symObj(28, "ASTMT", 0);
+	symArray[29] = new symObj(29, "Y", 0);
+	symArray[30] = new symObj(30, "OSTMT", 0);
+	symArray[31] = new symObj(31, "WSTMT", 0);
+	symArray[32] = new symObj(32, "FSTMT", 0);
+	symArray[33] = new symObj(33, "ELSE2", 0);
+	symArray[34] = new symObj(34, "ELIST", 0);
+	symArray[35] = new symObj(35, "ELIST2", 0);
+	symArray[36] = new symObj(36, "E", 0);
+	symArray[37] = new symObj(37, "Q", 0);
+	symArray[38] = new symObj(38, "T", 0);
+	symArray[39] = new symObj(39, "R", 0);
+	symArray[40] = new symObj(40, "F", 0);
+	symArray[41] = new symObj(41, "PERXPR", 0);
+	symArray[42] = new symObj(42, "FATOM", 0);
+	symArray[43] = new symObj(43, "OPADD", 0);
+	symArray[44] = new symObj(44, "OPMUL", 0);
 	
 
 }
