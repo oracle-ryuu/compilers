45df6b2e89edf36f996438a8741906bcb3931027
Jared Vanotterdyk <oracleryuu@csu.fullerton.edu>
2017-05-10 16:36:39 -0700

    treenode implemented

---
+++ Tree generator/Tree generator/TreeNode.h
@@ -0,0 +1,41 @@
+#pragma once
+#include<iostream>
+#include <string>
+#include "ruleobj.h"
+#include "symObj.h"
+using namespace std;
+
+class TreeNode
+{
+public:
+	TreeNode() {
+		setID(0);
+		setSymKind(0);
+		setSymID(0);
+		
+	}
+				
+	
+	TreeNode(int id, int sym_id, int isTerm, int rule ) {
+		setID(id);
+		setSymKind(sym_id);
+		setSymID(isTerm);
+	};
+
+private:
+	int ID;
+	int symID;
+	//token?
+	int symKind; // 0 for nonterminal, 1 for terminal
+	TreeNode* kids[10];
+	int ruleID;
+public:
+	void setKid(int slot, symObj child, int NodeID, ruleobj rule) { kids[slot] = create_Kid(child, NodeID, rule); }
+	void setID(int id) { ID = id; }
+	void setSymKind(int kind) { symKind = kind; }
+	void setSymID(int symid) { symID = symid; }
+	void setRuleID(int ruleid) { ruleID = ruleid; }
+
+};
+
+TreeNode* create_Kid(symObj child, int NodeID,ruleobj rule) { return new TreeNode(NodeID,child.getSymid(), child.getSymkind(), rule.getID() ); }
